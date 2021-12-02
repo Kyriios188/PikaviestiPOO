55ebac3c763181ad14ffeccf3aac75c2e9f98760
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-12-02 08:34:27 +0100

    Create ChatSystemController.java

---
+++ pikaviesti/src/ChatSystemController.java
@@ -0,0 +1,66 @@
+
+
+
+public class Chat System Controller {
+
+    public user local_user;
+
+
+    public user_id local_id;
+
+
+    public Chat System GUI creates and updates;
+
+
+    public Communication System receives;
+
+
+    public Chat System Model creates and uses;
+
+
+    public Clock uses;
+
+
+    public Database updates and queries;
+
+
+    public chat_history getChatHistory(user target_user) {
+    }
+
+
+    public boolean checkNameUnique(String name) {
+    }
+
+
+    public void notifyNameChange(user new_user) {
+    }
+
+
+    public void stampMessage(message message) {
+    }
+
+
+    public void updateGUI(message new_message) {
+    }
+
+
+    public void updateChatHistory(message new_message) {
+    }
+
+
+    public user findUser(String name) {
+    }
+
+
+    public void receiveNotification(user user) {
+    }
+
+
+    public void showUserList() {
+    }
+
+
+    public void createMessage(String input, String username) {
+    }
+
+}

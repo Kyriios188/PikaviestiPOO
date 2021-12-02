498cee3e767f333befec9b1bd43bba1bcd9bad48
Kyriios188 <robin.siim188@gmail.com>
2021-12-02 09:27:48 +0100

    jsp

--- pikaviesti/src/Starter.java
+++ pikaviesti/src/Starter.java
@@ -3,8 +3,7 @@ public class Starter {
 	public static void main(String args[]) {
 		
 		// Launch GUI
-		ChatSystemGUI gui = new ChatSystemGUI();
-		gui.createAndShowGUI();
+		ChatSystemGUI.createAndShowGUI();
 		
 	}
 }

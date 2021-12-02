819ee8399dab9d3221429b96588ad73566dd77c5
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-12-02 08:40:03 +0100

    Create User.java

---
+++ pikaviesti/src/User.java
@@ -0,0 +1,27 @@
+
+
+
+
+public class User {
+
+    public String name;
+
+
+    public user_id id;
+
+
+    public Chat System Model ;
+
+
+    public user_id getId() {
+    }
+
+
+    public String getName() {
+    }
+
+
+    public void changeName(String name) {
+    }
+
+}

f21749c90eec0cf3d0a71e04c7fb87e084d87ddd
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-12-02 08:53:04 +0100

    Update ChatSystemController.java

--- pikaviesti/src/ChatSystemController.java
+++ pikaviesti/src/ChatSystemController.java
@@ -1,59 +1,59 @@
 
 
 
-public class Chat System Controller {
+public class ChatSystemController {
 
-    public user local_user;
-
-
-    public user_id local_id;
+    //public user local_user;
 
 
-    public Chat System GUI creates and updates;
+    //public user_id local_id;
 
 
-    public Communication System receives;
+    //public ChatSystemGUI creates and updates;
 
 
-    public Chat System Model creates and uses;
+    //public CommunicationSystem receives;
 
 
-    public Clock uses;
+    //public ChatSystemModel creates and uses;
 
 
-    public Database updates and queries;
+    //public Clock uses;
 
 
-    public chat_history getChatHistory(user target_user) {
-    }
+    //public Database updates and queries;
 
 
-    public boolean checkNameUnique(String name) {
-    }
+    //public chat_history getChatHistory(user target_user) {
+    //}
 
 
-    public void notifyNameChange(user new_user) {
-    }
+    //public boolean checkNameUnique(String name) {
+    //}
 
 
-    public void stampMessage(message message) {
-    }
+    //public void notifyNameChange(user new_user) {
+    //}
 
 
-    public void updateGUI(message new_message) {
-    }
+    //public void stampMessage(message message) {
+    //}
 
 
-    public void updateChatHistory(message new_message) {
-    }
+    //public void updateGUI(message new_message) {
+    //}
 
 
-    public user findUser(String name) {
-    }
+    //public void updateChatHistory(message new_message) {
+    //}
 
 
-    public void receiveNotification(user user) {
-    }
+    //public user findUser(String name) {
+    //}
+
+
+    //public void receiveNotification(user user) {
+    //}
 
 
     public void showUserList() {

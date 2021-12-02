50808da937c55bd99dad177d4491270a16029338
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-12-02 08:22:49 +0100

    Update ChatSystemGUI.java

--- pikaviesti/src/ChatSystemGUI.java
+++ pikaviesti/src/ChatSystemGUI.java
@@ -2,4 +2,53 @@
 
 public class ChatSystemGUI {
 
+    public GUI user_gui_window;
+
+
+    public String local_message;
+
+
+    public void showChatHistory(String target_name) {
+    }
+
+
+    public String getUserMessage() {
+    }
+
+
+    public boolean chooseUserName(String name) {
+    }
+
+
+    public void startSession(String name) {
+    }
+
+
+    public void endSession(String name) {
+    }
+
+
+    public void changeDistantUsername(String old_name, String new_name) {
+    }
+
+
+    public void updateGUImessageReceived(String message_content, String sender_name, datetime time) {
+    }
+
+
+    public void updateGUImessageSent(String message_content, String dest_user, datetime time) {
+    }
+
+
+    public void showFailureToast() {
+    }
+
+
+    public void showSuccessToast() {
+    }
+
+
+    public void showUserList(String name_array) {
+    }
+
 }

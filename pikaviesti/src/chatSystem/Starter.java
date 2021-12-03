82da02b975aeba2d2f4c4c43d4e42f0f217f1ea2
Kyriios188 <robin.siim188@gmail.com>
2021-12-03 18:15:05 +0100

    only have to implement reaction to response to whatsyourname before testing

--- pikaviesti/src/chatSystem/Starter.java
+++ pikaviesti/src/chatSystem/Starter.java
@@ -1,12 +1,22 @@
 package chatSystem;
 
+import java.util.Random;
+
 import chatSystem.ChatSystemGUI;
 
 public class Starter {
 	public static void main(String args[]) {
+		//TODO: uncomment for production
 		ChatSystemGUI GUI = new ChatSystemGUI();
 		// Launch GUI
 		GUI.openLoginWindow();
 		
+		/*
+		ChatSystemController controller = new ChatSystemController(GUI);
+		int rand = new Random().nextInt(1000);
+		String random_name = Integer.toString(rand);
+		controller.setLocalUser(random_name);
+		controller.checkNameUnique(random_name);
+		*/
 	}
 }

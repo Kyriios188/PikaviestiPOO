f5fd01dd442669cdcc58fa37119c868b7511147a
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-12-03 16:04:20 +0100

    Create User.java

---
+++ pikaviesti/src/objects/User.java
@@ -0,0 +1,34 @@
+package objects;
+
+
+public class User {
+
+    private String name;
+
+
+    private int id;
+
+
+    public int getId() {
+    	return this.id;
+    }
+    
+    public void setId(int new_id) {
+    	this.id = new_id;
+    }
+
+
+    public String getName() {
+    	return this.name;
+    }
+    
+    public void setName(String new_name) {
+    	this.name = new_name;
+    }
+    
+    public User(String name, int id) {
+    	this.name = name;
+    	this.id = id;
+    }
+
+}

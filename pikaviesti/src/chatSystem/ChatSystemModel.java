6e3a50f021dd68cdd33cf68b8e679b42335324a6
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-12-03 16:04:03 +0100

    Create ChatSystemModel.java

---
+++ pikaviesti/src/chatSystem/ChatSystemModel.java
@@ -0,0 +1,35 @@
+package chatSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ChatSystemModel {
+
+    //public userlist connected_users;
+
+
+    //public List<user>  = new ArrayList<user> ();
+
+
+    public void getUserList() {
+    }
+
+
+    //public void addUser(user user) {
+    //}
+
+
+    //public void changeUserName(user u, String new_name) {
+    //}
+
+
+    //public user_id getIdFromName(String name) {
+    //}
+
+
+    public void deleteUser() {
+    }
+
+}

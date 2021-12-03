ccbcbfbc794048b0c7de76759a5c9f7e8fc0c0a2
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-12-03 16:04:05 +0100

    Create Starter.java

---
+++ pikaviesti/src/chatSystem/Starter.java
@@ -0,0 +1,12 @@
+package chatSystem;
+
+import chatSystem.ChatSystemGUI;
+
+public class Starter {
+	public static void main(String args[]) {
+		ChatSystemGUI GUI = new ChatSystemGUI();
+		// Launch GUI
+		GUI.openLoginWindow();
+		
+	}
+}

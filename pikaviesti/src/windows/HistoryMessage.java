a61741053c82eb0c7937b3b7d6aca13bc6a57fb2
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-12-03 16:57:59 +0100

    Updates GUI

--- pikaviesti/src/windows/HistoryMessage.java
+++ pikaviesti/src/windows/HistoryMessage.java
@@ -1,13 +1,18 @@
 package windows;
 
 import javax.swing.*;
+import chatSystem.ChatSystemGUI;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class HistoryMessage extends JDialog {
     private JPanel contentPane;
     private JButton buttonSEND;
     private JTextField textField1;
     private final DefaultListModel<String> listModel = new DefaultListModel<>();
-    private JList<String> list2;
+    private JList<String> listMessage;
     private JButton userListButton;
     private String messageText;
 
@@ -19,24 +24,39 @@ public class HistoryMessage extends JDialog {
 
         buttonSEND.addActionListener(e -> onSEND());
         userListButton.addActionListener(e -> onUserList());
-        list2 = new JList<>(listModel);
+        listMessage = new JList<>(listModel);
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        this.pack();
+        this.setVisible(true);
     }
 
     private void onSEND() {
         messageText = textField1.getText();
-        System.out.println("objects.Message : " + messageText);
+        System.out.println("Message : " + messageText);
         listModel.addElement(messageText);
         textField1.setText("");
     }
 
     private void onUserList() {
-        System.out.println("Show objects.User List");
+        System.out.println("Show User List");
+        ChatSystemGUI GUI = new ChatSystemGUI();
+        GUI.openUserList(null);
     }
 
-    public static void main(String[] args) {
-        HistoryMessage dialog = new HistoryMessage();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
+    private void onCancel() {
+        System.out.println("Dead");
+        dispose();
     }
+
 }
